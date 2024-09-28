dff038acabb92336aef547260be017ff3b7053c8
fugui <fugui0506@gmail.com>
2024-09-28 12:38:07 +0400

    优化

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from enum import Enum
 import os
 import subprocess
 import shutil
+from datetime import datetime
 
 # 设置 Flutter 项目路径
 PROJECT_DIR = os.path.expanduser("~/Documents/Projects/cgwallet")
@@ -15,12 +16,19 @@ IOS_DIR = os.path.join(PROJECT_DIR, "ios")
 ARCHIVE_PATH = os.path.join(BUILD_DIR, "ios/archive/Runner.xcarchive")
 # ios 在当前脚本目录下查找 ExportOptions.plist
 EXPORT_OPTIONS_PLIST = os.path.join(os.path.dirname(__file__), "ExportOptions.plist")
+# 构建时间
+BUILD_TIME = datetime.now().strftime("%Y%m%d_%H%M%S")
 
 # 定义一个枚举类
 class ENVIRONMENT(Enum):
     test = '--dart-define=ENVIRONMENT=test'
     pre = '--dart-define=ENVIRONMENT=pre'
     rel = '--dart-define=ENVIRONMENT=rel'
+
+class NAME(Enum):
+    test = 'test'
+    pre = 'pre'
+    rel = 'rel'
 
 def run_command(command, cwd=None):
     """
@@ -31,7 +39,7 @@ def run_command(command, cwd=None):
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd)
     stdout, stderr = process.communicate()
     if process.returncode != 0:
-        raise Exception(f"命令执行失败，错误信息: {stderr.decode('utf-8')}")
+        raise subprocess.CalledProcessError(process.returncode, command, output=stderr)
     print(stdout.decode('utf-8'))
 
 def clean_flutter_project():
@@ -56,15 +64,15 @@ def build_flutter_ipa(environment: ENVIRONMENT):
     """
     构建iOS版本的Flutter项目，使用发布模式并禁用代码签名。
     """
-    run_command("flutter build ios --release --no-codesign %s" % (environment.value), cwd=PROJECT_DIR)
+    run_command("flutter build ios -v --release --no-codesign %s" % (environment.value), cwd=PROJECT_DIR)
 
-def create_xcode_archive(environment: ENVIRONMENT):
+def create_xcode_archive():
     """
     创建Xcode归档文件并使用指定的ExportOptions.plist导出IPA文件。
     """
     # 检查ExportOptions.plist文件是否存在
     if not os.path.exists(EXPORT_OPTIONS_PLIST):
-        raise Exception(f"ExportOptions.plist文件未找到: {EXPORT_OPTIONS_PLIST}")
+        raise FileNotFoundError(f"ExportOptions.plist文件未找到: {EXPORT_OPTIONS_PLIST}")
 
     # 使用xcodebuild命令创建项目归档和导出IPA文件
     run_command(f"xcodebuild -workspace ios/Runner.xcworkspace -scheme Runner -configuration Release archive -archivePath {ARCHIVE_PATH} -destination 'generic/platform=iOS'", cwd=PROJECT_DIR)
@@ -72,16 +80,16 @@ def create_xcode_archive(environment: ENVIRONMENT):
 
     # 重命名IPA文件
     old_ipa_path = os.path.join(OUTPUT_DIR, 'cgwallet.ipa')
-    new_ipa_path = os.path.join(OUTPUT_DIR, 'cgwallet%s.ipa' % (environment.value))
+    new_ipa_path = os.path.join(OUTPUT_DIR, 'cgwallet_%s_%s.ipa' % (NAME.test.value, BUILD_TIME))
     os.rename(old_ipa_path, new_ipa_path)
 
 def build_flutter_apk(environment: ENVIRONMENT):
     """
     构建安卓版本的Flutter项目，使用发布模式。
     """
-    run_command("flutter build apk --release %s"  % (environment.value), cwd=PROJECT_DIR)
+    run_command("flutter build apk -v --release %s"  % (environment.value), cwd=PROJECT_DIR)
 
-def copy_apk_to_output(environment: ENVIRONMENT):
+def copy_apk_to_output():
     """
     将生成的APK复制到指定的输出目录。
     """
@@ -98,12 +106,12 @@ def copy_apk_to_output(environment: ENVIRONMENT):
         shutil.copy(apk_file, OUTPUT_DIR)
         print(f"成功复制APK文件到 {OUTPUT_DIR}")
     else:
-        raise Exception(f"找不到生成的APK文件: {apk_file}")
+        raise FileNotFoundError(f"找不到生成的APK文件: {apk_file}")
     
     # 重命名IPA文件
-    old_ipa_path = os.path.join(OUTPUT_DIR, 'app-release.apk')
-    new_ipa_path = os.path.join(OUTPUT_DIR, 'app-release%s.apk' % (environment.value))
-    os.rename(old_ipa_path, new_ipa_path)
+    old_apk_path = os.path.join(OUTPUT_DIR, 'app-release.apk')
+    new_apk_path = os.path.join(OUTPUT_DIR, 'cgwallet_%s_%s.apk' % (NAME.test.value, BUILD_TIME))
+    os.rename(old_apk_path, new_apk_path)
 
 def main():
     """
@@ -116,13 +124,12 @@ def main():
         install_cocoapods()
 
         build_flutter_ipa(ENVIRONMENT.test)
-        create_xcode_archive(ENVIRONMENT.test)
+        create_xcode_archive()
         print("成功创建IPA%s文件。" % (ENVIRONMENT.test.value))
 
         build_flutter_apk(ENVIRONMENT.test)
-        copy_apk_to_output(ENVIRONMENT.test)
+        copy_apk_to_output()
         print("成功创建并复制APK%s文件。" % (ENVIRONMENT.test.value))
-
     except Exception as e:
         print(f"错误: {e}")
 
